b73ab1a69c6cd0994d6ca934398018f2e04f3056
nakahara yasuhiro <nakaharayasuhiro@nakaharanoMacBook-Air.local>
2026-03-15 14:59:23 +0900

    refactor: remove InterviewSlot model and use MeetingSlot

--- app/models/interview_slot.rb
+++
@@ -1,8 +0,0 @@
-class MeetingSlot < ApplicationRecord
-  belongs_to :teacher
-  belongs_to :schedule
-  has_many :assignments
-  has_many :family_unavailabilities
-
-  enum :status, { available: 0, reserved: 1, blocked: 2 }
-end
