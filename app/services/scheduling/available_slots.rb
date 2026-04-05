b0904969ee33150aa53e3a6c1715054352271930
nakahara yasuhiro <nakaharayasuhiro@nakaharanoMacBook-Air.local>
2026-04-05 15:19:45 +0900

    feature:取得可能slot

--- app/services/scheduling/available_slots.rb
+++ app/services/scheduling/available_slots.rb
@@ -1,4 +1,18 @@
 module Scheduling
   class AvailableSlots
+    def initialize (schedule)
+        @schedule = schedule
+    end
+
+    def call(group)
+        teacher_ids = group.map do |g|
+            room_type = g[:type] == :support ? "support" : "normal"
+            g[:child].class_rooms.where(room_type: room_type).first&.teacher_id
+        end .compact
+
+        MeetingSlot.where(schedule: @schedule)
+                   .where(teacher_id: teacher_ids)
+                   .where.not(id: Assignment.select(:meeting_slot_id))
+    end
   end
 end
