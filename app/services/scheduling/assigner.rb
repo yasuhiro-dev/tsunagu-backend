ba533f2ffa8acc02a3132209686d942dc5cc9735
nakahara yasuhiro <nakaharayasuhiro@nakaharanoMacBook-Air.local>
2026-04-06 21:02:50 +0900

    feature:割当

--- app/services/scheduling/assigner.rb
+++ app/services/scheduling/assigner.rb
@@ -1,4 +1,18 @@
 module Scheduling
   class Assigner
+    def call (slots, group)
+      group.each do |g|
+      room_type = g[:type] == :support ? "support" : "normal"
+      teacher_id = g[:child].class_rooms
+                            .where(room_type: room_type)
+                            .first&.teacher_id
+      slot = slots.find { |s|s.teacher_id == teacher_id }
+      next if slot.nil?
+      Assignment.create!(
+        child_id: g[:child].id,
+        meeting_slot_id: slot.id
+      )
+      end
+    end
   end
 end
