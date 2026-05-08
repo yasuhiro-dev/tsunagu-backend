a1a5ebfcee9f5ce12fd787052927e9808084ded6
nakahara yasuhiro <nakaharayasuhiro@nakaharanoMacBook-Air.local>
2026-05-08 19:44:12 +0900

    保護者の登録API

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,7 +1,7 @@
 class Child < ApplicationRecord
   belongs_to :family
   has_many :assignments
-  belongs_to :schedule
+ belongs_to :schedule, optional: true
   has_many :child_class_rooms
   has_many :class_rooms, through: :child_class_rooms
 end
