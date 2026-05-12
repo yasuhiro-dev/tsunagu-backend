023fee77ad9b0db6a2ba5c517ca08ce246985a29
nakahara yasuhiro <nakaharayasuhiro@nakaharanoMacBook-Air.local>
2026-05-12 22:18:40 +0900

    兄弟の時間のバグ

--- app/services/scheduling/time_filter.rb
+++ app/services/scheduling/time_filter.rb
@@ -1,11 +1,13 @@
 module Scheduling
-    class TimeFilter
-        def call(slots, group)
-            unavailable_slots_ids = group.first[:child]
-                                         .family
-                                         .family_unavailabilities
-                                         .pluck(:meeting_slot_id)
-            slots.reject { |s|unavailable_slots_ids.include?(s.id) }
-        end
+  class TimeFilter
+    def call(slots, group)
+      unavailable_slot_ids = group.map { |g| g[:child] }
+                                  .flat_map { |child| child.family.family_unavailabilities.pluck(:meeting_slot_id) }
+                                  .uniq
+
+      unavailable_times = MeetingSlot.where(id: unavailable_slot_ids).pluck(:start_at)
+
+      slots.reject { |s| unavailable_times.include?(s.start_at) }
     end
+  end
 end
