54c02fae71e363aa43311cced1a4adc2e9d5ad9a
nakahara yasuhiro <nakaharayasuhiro@nakaharanoMacBook-Air.local>
2026-05-17 16:05:18 +0900

    管理者画面の新規登録の先生の表示のバグ修正

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::UsersController < ApplicationController
   def create_parent
+    schedule = Schedule.order(created_at: :desc).first
     ActiveRecord::Base.transaction do
-      # ① 保護者ユーザーを作成
       @user = User.new(parent_params)
       @user.role = "parent"
 
@@ -10,21 +10,15 @@ class Api::V1::UsersController < ApplicationController
         raise ActiveRecord::Rollback
       end
 
-# ② 自動作成されたfamilyを取得して名前を更新
 @family = @user.family
 @family.update(name: params[:family_name])
 
-
-      # ③ 児童を複数保存
       children_params_list.each do |child_attrs|
-        # 児童を保存
-        child = Child.new(name: child_attrs[:name], family_id: @family.id)
+        child = Child.new(name: child_attrs[:name], family_id: @family.id, schedule_id: schedule&.id)
         unless child.save
           render json: { errors: child.errors.full_messages }, status: :unprocessable_entity
           raise ActiveRecord::Rollback
         end
-
-        # child_class_rooms に保存
         child_class_room = ChildClassRoom.new(child_id: child.id, class_room_id: child_attrs[:class_room_id])
         unless child_class_room.save
           render json: { errors: child_class_room.errors.full_messages }, status: :unprocessable_entity
--- app/services/scheduling/time_filter.rb
+++ app/services/scheduling/time_filter.rb
@@ -1,12 +1,10 @@
 module Scheduling
   class TimeFilter
     def call(slots, group)
-
       unavailable_slots_ids = group.map { |g| g[:child] }
                                    .flat_map { |child| child.family.family_unavailabilities.pluck(:meeting_slot_id) }
                                    .uniq
       slots.reject { |s| unavailable_slots_ids.include?(s.id) }
-
     end
   end
 end
