4ef60d6672932021a78339fd61002d1d37baba8b
nakahara yasuhiro <nakaharayasuhiro@nakaharanoMacBook-Air.local>
2026-06-27 10:25:11 +0900

    パスワードリセット機能を修正

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,8 @@
 class UserMailer < ApplicationMailer
-    def password_reset(user)
-      @user = user
-      mail to: user.email, subject: "Password reset"
+     def password_reset
+    @user = params[:user]
+    @token = @user.reset_token
+    @url = "https://tsunagu-app.com/password_reset_confirm?token=#{@token}"
+    mail(to: @user.email_address, subject: "パスワードの再設定")
     end
 end
