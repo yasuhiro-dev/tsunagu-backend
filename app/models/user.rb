e2520807378ec80e361aba15c2365cac64fb26a9
nakahara yasuhiro <nakaharayasuhiro@nakaharanoMacBook-Air.local>
2026-06-28 14:15:37 +0900

    def authenticatedの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,12 @@ class User < ApplicationRecord
     validates :role, presence: true, inclusion: { in: %w[teacher parent admin] }
     has_one :teacher, dependent: :destroy
     has_one :family, dependent: :destroy
+    
+def authenticated?(attribute, token)
+  digest = send("#{attribute}_digest")
+  return false if digest.nil?
+  BCrypt::Password.new(digest).is_password?(token)
+end
 
     def User.new_token
     SecureRandom.urlsafe_base64
