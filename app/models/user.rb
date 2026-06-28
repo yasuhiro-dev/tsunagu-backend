e05317ee493d9e1789944652818d1690615de496
nakahara yasuhiro <nakaharayasuhiro@nakaharanoMacBook-Air.local>
2026-06-28 11:21:53 +0900

    create_reset_digestを1回のupdateにまとめた

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,12 +16,13 @@ class User < ApplicationRecord
     BCrypt::Password.create(string, cost: cost)
     end
 after_create :create_role_record
-
   def create_reset_digest
-    self.reset_token = User.new_token
-    update_attribute(:reset_digest,  User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
-  end
+  self.reset_token = User.new_token
+  update(
+    reset_digest: User.digest(reset_token),
+    reset_sent_at: Time.zone.now
+  )
+end
 
   def send_password_reset_email
     UserMailer.with(user: self).password_reset.deliver_now
