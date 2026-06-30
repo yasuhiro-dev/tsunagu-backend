c7c5ac323fdad6e4bc1ca8a46f7b74ad485441dc
nakahara yasuhiro <nakaharayasuhiro@nakaharanoMacBook-Air.local>
2026-06-30 22:34:21 +0900

    登録画面validation

--- app/models/family.rb
+++ app/models/family.rb
@@ -2,4 +2,5 @@ class Family < ApplicationRecord
     has_many :children, dependent: :destroy
     has_many :family_unavailabilities, dependent: :destroy
     belongs_to :user
+    validates :name, presence: { message: "を入力してください" }
 end
