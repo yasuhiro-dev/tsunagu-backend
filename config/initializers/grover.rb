7b7dfbaffb620c7a8bd8a96e3ac77ca3072bffb4
Nakahara Yasuhiro <yattinx@gmail.com>
2026-08-02 09:55:47 +0900

    PDFがうまく出力されないバグを修正

--- config/initializers/grover.rb
+++ config/initializers/grover.rb
@@ -1,5 +1,7 @@
 Grover.configure do |config|
   config.options = {
+    executable_path: "/usr/bin/chromium",
     launch_args: [ "--no-sandbox", "--disable-setuid-sandbox", "--font-render-hinting=medium", "--lang=ja" ]
+
   }
 end
