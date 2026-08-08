b9c7ece69c286c866e142f4659efe8f1df4ef613
Nakahara Yasuhiro <yattinx@gmail.com>
2026-08-08 14:59:55 +0900

    rubocop修正

--- spec/requests/api/v1/password_resets_spec.rb
+++ spec/requests/api/v1/password_resets_spec.rb
@@ -2,7 +2,6 @@ require "rails_helper"
 
 # PasswordResetsに関するテスト（リクエストスペック）
 RSpec.describe "Api::V1::PasswordResets", type: :request do
-
   # POSTメソッドのテスト
   describe "POST api_v1_password_resets_path" do
     let(:user) { create(:user) }
@@ -29,7 +28,7 @@ RSpec.describe "Api::V1::PasswordResets", type: :request do
     before { user.create_reset_digest }
     # userのリセットトークンとpasswordを一緒に持たせる
     subject { patch(api_v1_password_resets_token_path(user.reset_token), params: { user: { password: "newpassword123", password_confirmation: "newpassword123" } }) }
-    
+
     context "DBに保存されたreset_digestと一致するUserがあれば" do
       # 外側の let(:user) をそのまま使う
       it_behaves_like "成功する", :ok
@@ -38,25 +37,25 @@ RSpec.describe "Api::V1::PasswordResets", type: :request do
       subject { patch(api_v1_password_resets_token_path("nottoken"), params: { user: { password: "newpassword123", password_confirmation: "newpassword123" } }) }
       it_behaves_like "成功する", :not_found
     end
-  
+
     context "トークンの有効期限が切れていなければ" do
       # 外側の before { user.create_reset_digest }をそのまま使う
       it_behaves_like "成功する", :ok
     end
 
     context "トークンの有効期限が切れていれば" do
-      before { user.update_column(:reset_sent_at,3.hours.ago) }
-      it_behaves_like "成功する", :unprocessable_entity 
+      before { user.update_column(:reset_sent_at, 3.hours.ago) }
+      it_behaves_like "成功する", :unprocessable_entity
     end
-    
+
     context "パスワードが空だったら" do
       subject { patch(api_v1_password_resets_token_path(user.reset_token), params: { user: { password: "", password_confirmation: "" } }) }
-      it_behaves_like "成功する", :unprocessable_entity 
+      it_behaves_like "成功する", :unprocessable_entity
     end
 
     context "パスワードがバリデーションエラーだったら" do
       subject { patch(api_v1_password_resets_token_path(user.reset_token), params: { user: { password: "1234", password_confirmation: "1234" } }) }
-      it_behaves_like "成功する", :unprocessable_entity 
+      it_behaves_like "成功する", :unprocessable_entity
     end
   end
 end
