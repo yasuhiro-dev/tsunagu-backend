6351ce7cf0fb9f18fe451fb3da5d1e0a2dd772d9
Nakahara Yasuhiro <yattinx@gmail.com>
2026-08-15 07:52:10 +0900

    面談の調節機能と児童関連情報（兄弟・特別支援・時間制約）の取得

--- app/models/child.rb
+++ app/models/child.rb
@@ -5,4 +5,29 @@ class Child < ApplicationRecord
   has_many :child_class_rooms, dependent: :destroy
   has_many :class_rooms, through: :child_class_rooms
   validates :name, presence: { message: "を入力してください" }
+
+  # 兄弟を取得（自分は含まない）
+  def siblings
+    family.children.where.not(id:id)
+  end
+  # その児童のクラスから、面談表・クラス名・担任名・児童名を取得
+  def related_schedules
+    class_rooms.map do |class_room|
+    teacher = class_room.teacher
+    {
+      teacher_name: teacher.name,
+      class_room_name: class_room.classname,
+      # slotsは複数なので1つのslotに絞ってから（N+1対策でassignmentとchildをincludes）
+      slots: MeetingSlot.where(teacher:teacher).includes(assignments: :child).map{|slot| {
+        # オブジェクトはmapを使うと再設定
+        id: slot.id,
+        start_at: slot.start_at,
+        end_at: slot.end_at,
+        status: slot.status,
+        # assignmentsでアソシエーションが複数なので.firstにしてから取り出す
+        child_name: slot.assignments.first&.child&.name
+        };
+      }}
+  end
+  end
 end
