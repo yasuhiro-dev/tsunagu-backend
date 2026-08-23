72717f076a94101a3361bb3471a4b5adc74b530b
Nakahara Yasuhiro <yattinx@gmail.com>
2026-08-24 06:17:13 +0900

    google連携の確認・メール本文修正

--- app/services/google_oauth_client.rb
+++ app/services/google_oauth_client.rb
@@ -1,11 +1,12 @@
 class GoogleOauthClient
+    # googleのトークンを取得メソッド
     def self.build
     OAuth2::Client.new(
-      Rails.application.credentials.google[:client_id],
-      Rails.application.credentials.google[:client_secret],
-      site: "https://accounts.google.com",
-      authorize_url: "/o/oauth2/auth",
-      token_url: "/o/oauth2/token"
+      Rails.application.credentials.google[:client_id], # googleにこのアプリを認識させる
+      Rails.application.credentials.google[:client_secret], # このアプリの証明書
+      site: "https://accounts.google.com", # googleのどのサイトに飛ばすか
+      authorize_url: "/o/oauth2/auth", # 同意画面（ユーザーがブラウザで見る）
+      token_url: "/o/oauth2/token" # トークン取得（サーバー間通信、ユーザーには見えない）
     )
   end
 end
