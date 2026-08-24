f25ffa27a3df9ddc89ca01c4c9f17c6713013b27
Nakahara Yasuhiro <yattinx@gmail.com>
2026-08-24 22:11:15 +0900

    割当管理ページのUI修正

--- app/services/gmail_service.rb
+++ app/services/gmail_service.rb
@@ -6,17 +6,19 @@ class GmailService
   def initialize(user)
     @user = user
   end
-
+  #
   def send_email(to:, subject:, body:)
+    # トークンがあるか確認する
     ensure_valid_token
-
+    # データ構造を作成
     raw_message = build_raw_message(to: to, subject: subject, body: body)
-
+    # アクセストークンを使ってgmailAPIへリクエストする
     response = access_token.post(
       GMAIL_SEND_URL,
       headers: { "Content-Type" => "application/json" },
       body: { raw: raw_message }.to_json
     )
+    # リクエスト受け付けを確認
     JSON.parse(response.body)
   end
 
@@ -29,28 +31,31 @@ class GmailService
     # もし切れているならリフレッシュトークンを渡す
     refresh_access_token
   end
-
+  # リフレッシュトークンを使ってトークンを発行する
   def refresh_access_token
     token = GoogleOauthClient.build.get_token(
       grant_type: "refresh_token",
       refresh_token: @user.google_refresh_token
     )
-
     @user.update(
       google_access_token: token.token,
       google_token_expires_at: Time.at(token.expires_at)
     )
   end
+  # .post呼び出し時に自動でAuthorizationヘッダーを付与
   def access_token
     OAuth2::AccessToken.new(GoogleOauthClient.build, @user.google_access_token)
   end
+  # メールのデータ構造を作成
   def build_raw_message(to:, subject:, body:)
     mail = Mail.new do
       to           to
       subject      subject
       body         body
+      # メールの本文がテキストで日本語として表示される設定
       content_type "text/plain; charset=UTF-8"
     end
+    # JSON経由で壊れずに送るため
     Base64.urlsafe_encode64(mail.to_s)
   end
 end
