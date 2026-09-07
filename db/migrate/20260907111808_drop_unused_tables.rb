1236a86f12b1437bf51df1e22d2679bd9d05c275
Nakahara Yasuhiro <yattinx@gmail.com>
2026-09-07 20:23:30 +0900

    rubocop 修正

--- db/migrate/20260907111808_drop_unused_tables.rb
+++ db/migrate/20260907111808_drop_unused_tables.rb
@@ -6,7 +6,7 @@ class DropUnusedTables < ActiveRecord::Migration[8.1]
       t.datetime "updated_at", null: false
       t.string "user_agent"
       t.bigint "user_id", null: false
-      t.index ["user_id"], name: "index_sessions_on_user_id"
+      t.index [ "user_id" ], name: "index_sessions_on_user_id"
     end
 
     drop_table :children_teachers do |t|
@@ -14,8 +14,8 @@ class DropUnusedTables < ActiveRecord::Migration[8.1]
       t.datetime "created_at", null: false
       t.bigint "teacher_id"
       t.datetime "updated_at", null: false
-      t.index ["child_id"], name: "index_children_teachers_on_child_id"
-      t.index ["teacher_id"], name: "index_children_teachers_on_teacher_id"
+      t.index [ "child_id" ], name: "index_children_teachers_on_child_id"
+      t.index [ "teacher_id" ], name: "index_children_teachers_on_teacher_id"
     end
   end
 end
